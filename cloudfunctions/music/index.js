8242f674d13c4a28b36bdd9bcb63d30160828b5b
Demon-Qin <1792357514@qq.com>
2021-01-29 23:36:15 +0800

    播放页面ok

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -34,5 +34,10 @@ app.router('musiclist',async (ctx, next) => {
   console.log('####' + res)
   ctx.body = res.data
 })
+app.router('musicUrl', async (ctx, next) => {
+  const res = await axios.get(`${BASE_URL}/song/url?id=${event.musicId}`)
+  ctx.body = res.data
+
+})
 return app.serve()
 }
