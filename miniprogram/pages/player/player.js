5897e734fa89492fef3ab229e4e2de6f482478b6
Demon-Qin <1792357514@qq.com>
2021-01-30 21:38:00 +0800

    播放歌曲交互

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,47 +1,90 @@
-// pages/player/player.js
 let musiclist = []
-// 正在播放歌曲index
+//正在播放的歌曲 index
 let playingIndex = 0
+ const backgroundAudioManager =wx.getBackgroundAudioManager()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    picUrl: ''
-
+    picUrl: '',
+    isPlaying:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    // console.log(options)
     console.log(options.musicId, typeof (options.musicId))
-    playingIndex = options.index
+    playingIndex = options.index 
     musiclist = wx.getStorageSync('musiclist')
-    this._loadMusicDetall(options.musicId)
+    this._loadMusicDetail(options.musicId)
   },
-  _loadMusicDetall(musicId){
-    console.log(playingIndex)
-    let music = musiclist[playingIndex]
-    console.log(musiclist)
+
+  _loadMusicDetail(musicId){
+    let music =  musiclist[playingIndex]
     console.log(music)
+
     wx.setNavigationBarTitle({
       title: music.name,
     })
     this.setData({
-      picUrl: music.al.picUrl
+      picUrl:music.al.picUrl
     })
     wx.cloud.callFunction({
       name:'music',
       data:{
         musicId,
-        $url:'musicUrl',
+        $url:'musicUrl'
       }
     }).then((res) => {
       console.log(res)
-    })
+      const url = res.result.data[0].url
+      if(url === null){
+        wx.showToast({
+          title: '没有权限播放',
+        })
+        backgroundAudioManager.pause()
+        this.setData({
+          isPlaying: false
+        })
+        return
+      }
 
+    backgroundAudioManager.src = url
+    backgroundAudioManager.title = music.name
+    backgroundAudioManager.coverImgUrl = music.al.picUrl
+    backgroundAudioManager.singer = music.ar[0].name
+      this.setData({
+        isPlaying:true
+      })
+    })
+  },
+  togglePlaying(){
+    if(this.data.isPlaying){
+      backgroundAudioManager.pause()
+    }else{
+      backgroundAudioManager.play()
+    }
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
+  },
+  onPrev(){
+    playingIndex--
+    if(playingIndex === 0){
+      playingIndex = musiclist.length - 1
+    }  
+    this._loadMusicDetail(musiclist[playingIndex].id)
+  
+  },
+  onNext(){
+    playingIndex++
+    if(playingIndex === musiclist.length){
+      playingIndex = 0
+    }  
+    this._loadMusicDetail(musiclist[playingIndex].id)
   }
 })
