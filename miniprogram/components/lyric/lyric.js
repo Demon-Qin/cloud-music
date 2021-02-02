de9a1c347891a691a87386606a95c094d81c47fb
Demon-Qin <1792357514@qq.com>
2021-02-03 00:08:21 +0800

    歌词滚动

--- miniprogram/components/lyric/lyric.js
+++ miniprogram/components/lyric/lyric.js
@@ -1,4 +1,4 @@
-let l
+let lyricHeight = 0
 Component({
   /**
    * 组件的属性列表
@@ -10,12 +10,13 @@ Component({
     },
     lyric:String,
   },
-
   /**
    * 组件的初始数据
    */
   data: {
-   lyrics:[]
+   lyrics:[],
+   nowLyricIndex: 0,//当前选中的歌词的索引
+   scrollTop: 0,//滚动条滚动的高度
   },
    observers:{
       lyric(lrc){
@@ -31,22 +32,60 @@ Component({
         console.log(lrc)
       }
    },
+   lifetimes: {
+       ready() {
+         wx.getSystemInfo({
+           success: (res) => {
+             lyricHeight = res.screenWidth / 750*64
+           },
+         })
+       }
+   },
   /**
    * 组件的方法列表
    */
   methods: {
+    update(currentTime) {
+         let lrcList = this.data.lyrics
+         if (lrcList.length == 0) {
+           return
+         }
+         if (currentTime > lrcList[lrcList.length-1].time) {
+           if(this.data.nowLyricIndex != -1) {
+             this.setData({
+               nowLyricIndex: -1,
+               scrollTop: lrcList.length * lyricHeight
+             })
+           }
+         }
+         for(let i=0, len = lrcList.length; i<len; i++) {
+           if (currentTime <= lrcList[i].item) {
+             this.setData({
+               nowLyricIndex: i-1,
+               scrollTop:(i-1)*lyricHeight
+             })
+             break
+           }
+         }
+    },
       _parseLyric(lyricSrc){
          let lines = lyricSrc.split('\n')
-         let _lyris = []
+         let _lrcList = []
           lines.forEach((elem) =>{
-            const lrc = elem.split(']')[1]
-            _lyris.push(lrc)
-          })
-
-
-
-         this.setData({
-           lyrics :_lyris
+            let time = elem.match(/\[(\d{2,}):(\d{2})(?:\.(\d{2,3}))?]/g)
+            if(time !=null) {
+              let lrc = elem.split(time)[1]
+              let timeReg = time[0].match(/(\d{2,}):(\d{2})(?:\.(\d{2,3}))?/)
+              console.log(timeReg)
+              let time2Seconds = parseInt(timeReg[1] * 60 +parseInt(timeReg[2]) + parseInt(timeReg[3]))/1000
+              _lrcList.push({
+                lrc,
+                time: time2Seconds,
+              })
+            }
+            })
+           this.setData({
+           lyrics :_lrcList
          })
 
       }
